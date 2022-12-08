671e8991ff1657e8a7b0c2d4d0ba2e7af5f9c558
Carlos Salguera <carlos.e.salguera@gmail.com>
2022-12-07 23:24:51 -0500

    finish lab

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// Cached Element References:
+const input = document.getElementById('input-item')
+const submit = document.getElementById('submit-button')
+const list = document.getElementById('todo-list')
+
+// // button event listener -> click
+// // create li element -> createElement('li')
+// // set text of li element to text written in imput element -> textContent
+// // reset text of input to empty string -> ('')
+// // add li element to page inside ul element -> ul appendChild li
+
+submit.addEventListener('click', () => {
+  const listItem = document.createElement('li')
+  listItem.textContent = input.value
+  input.value = ('')
+  list.appendChild(listItem)
+})
